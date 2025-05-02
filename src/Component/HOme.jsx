350d1af56bb809fc155b2befa3f3ddc6bef06ae4
AarohiSaxena10 <aarohisaxena1014@gmail.com>
2025-05-02 11:59:31 +0530

    changes made

--- src/Component/HOme.jsx
+++ src/Component/HOme.jsx
@@ -3,16 +3,14 @@ import Display from './Display';
 import Footer from './Footer';
 
 const HOme = () => {
-  const [latestcollection, setLatest] = useState(null);
-  const [bestseller, setBest] = useState(null);
   const [response, setRes] = useState("");
+
   useEffect(() => {
     async function getdata() {
       try {
         const data = await fetch('https://dummyjson.com/products?limit=100');
         const res = await data.json();
         setRes(res);
-
       }
       catch (err) {
         alert(err);
@@ -20,6 +18,7 @@ const HOme = () => {
     }
     getdata();
   }, []);
+
   const shuffle = useMemo(() => {
     if (!response || !Array.isArray(response.products)) {
       return [];
@@ -28,11 +27,6 @@ const HOme = () => {
     const shuffledClothes = copied.sort(() => 0.8 - Math.random());
     return shuffledClothes?.slice(14, 24);
   }, [response])
-
-  useEffect(() => {
-    if (shuffle.length)
-      setLatest(shuffle);
-  }, [shuffle])
 
   const bestshuffle = useMemo(() => {
     if (!response || !Array.isArray(response.products)) {
@@ -43,33 +37,31 @@ const HOme = () => {
     return best;
   }, [response])
 
-  useEffect(() => {
-    if (bestshuffle.length)
-      setBest(bestshuffle);
-  }, [bestshuffle]);
-
   return (
     <div className='flex mt-5 flex-col h-full items-center '>
-      <div className='bg-gray-300 h-[700px] w-[85%]'>
+      <div className='bg-gray-300 h-[700px] w-[85%]' />
 
-      </div>
       <p className='text-7xl font-bold mt-16 text-left'>Latest Collection </p>
       <div className='flex flex-wrap mt-5 justify-center'>
-        {latestcollection?.map((item, index) => (
-          <Display item={item} id={item.id} key={index} />
-        ))}
+        {shuffle && shuffle.length > 0 ? (
+          shuffle?.map((item, index) => (
+            <Display item={item} id={item.id} key={index} />
+          ))
+        ) : (<Shimmer />)
+        }
       </div>
+
       <p className='text-7xl font-bold mt-16 '>Best Seller</p>
       <div className='flex mt-5 justify-center'>
-        {bestseller && bestseller.length > 0 ? (
-          bestseller?.map((item, index) => (
+        {bestshuffle && bestshuffle.length > 0 ? (
+          bestshuffle?.map((item, index) => (
             <Display item={item} id={item.id} key={index} />
           ))) : (
-
-          <p>Loading</p>
+          <Shimmer />
         )
         }
       </div>
+      
       <Footer />
     </div>
   )
