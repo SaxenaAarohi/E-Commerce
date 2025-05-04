da2b9318af20d1969f6616d65c1fc703769ff856
AarohiSaxena10 <aarohisaxena1014@gmail.com>
2025-05-04 20:51:16 +0530

    adjust destials

--- src/Component/Details.jsx
+++ src/Component/Details.jsx
@@ -33,7 +33,8 @@ const Details = () => {
 
     return (
         <div className='h-screen select-none flex flex-col items-center cursor-pointer w-screen'>
-            <div className='flex justify-between h-full w-[90%] '>
+
+            <div className='flex md:flex-row  flex-col  md:justify-between items-center md:items-start h-full md:w-[90%] '>
 
                 <div className=' md:flex md:flex-col justify-center ml-10 gap-5 h-full w-[10%]  hidden'>
                     {images && images.length > 0 ?
@@ -48,60 +49,60 @@ const Details = () => {
                     }
                 </div>
 
-                <div className='  mt-28 h-full w-[30%]'>
+                <div className='  mt-16 md:mt-28 h-full w-full md:w-[30%]'>
 
-                    <div className='flex flex-col'>
-                        {mainimgurl && (<img className='h-[20%] md:h-[60%] w-full' src={mainimgurl}></img>)}
-                           <div className=' flex flex-wrap justify-center ml-4 md:ml-10 gap-5 h-[10%]  w-[50%] md:w-[10%] md:hidden '>
-                    {images && images.length > 0 ?
-                        images?.map((item) =>
-                            <img className='hover:border hover:border-indigo-500' onClick={() => setMainimg(item)} src={item} key={item.id} />
-                        )
-                        : (
-                            <>
-                                <p>Loading...</p>
-                            </>
-                        )
-                    }
-                </div>
+                    <div className='flex items-center flex-col'>
+                        {mainimgurl && (<img className='h-full w-full  ' src={mainimgurl}></img>)}
+                        <div className=' flex justify-center  gap-3 h-[14%] mt-5 md:mt-0  w-[20%] md:w-[10%] md:hidden '>
+                            {images && images.length > 0 ?
+                                images?.map((item) =>
+                                    <img className='hover:border hover:border-indigo-500' onClick={() => setMainimg(item)} src={item} key={item.id} />
+                                )
+                                : (
+                                    <>
+                                        <p>Loading...</p>
+                                    </>
+                                )
+                            }
+                        </div>
                     </div>
 
-
-
                 </div>
 
-                <div className='flex overflow-y-auto scrollbar-hide flex-col mt-16  w-[75%] md:w-[50%]'>
+                <div className='flex md:overflow-y-auto md:scrollbar-hide flex-col mt-10 md:mt-16  w-[75%] md:w-[50%]'>
 
                     <p className='font-sans  font-bold  text-3xl md:text-7xl'>{title}</p>
 
-                    <p className='text-left mt-5 md:mt-12  text-xl md:text-4xl '>{description}</p>
+                    <p className='text-left mt-5 md:mt-12 md:block hidden text-xl md:text-4xl '>{description}</p>
+                    <div className='md:block flex gap-5'>
+                        <p className='text-2xl md:text-5xl pt-2 md:pt-5 text-green-600'>{brand}</p>
 
-                    <p className='text-2xl md:text-5xl pt-2 md:pt-5 text-green-600'>{brand}</p>
+                        <p className='flex bg-green-600 mt-0 md:mt-7  text-center text-xl md:text-3xl px-4 py-1 md:py-3 font-bold rounded-3xl w-24 md:w-32'>{rating}<img className='h-9 mr-4 w-9' src='https://png.pngtree.com/png-vector/20240125/ourmid/pngtree-white-star-png-png-image_11494271.png' /></p>
+                    </div>
 
-                    <p className='flex bg-green-600 mt-7 text-center text-xl md:text-3xl px-4 py-1 md:py-3 font-bold rounded-3xl w-24 md:w-32'>{rating}<img className='h-9 mr-4 w-9' src='https://png.pngtree.com/png-vector/20240125/ourmid/pngtree-white-star-png-png-image_11494271.png' /></p>
 
                     <p className='text-green-600 pt-3 md:pt-6  text-xl md:text-3xl font-semibold'> Special price</p>
 
-                    <div className='flex md:flex-row flex-col items-center gap-5'>
-                        <p className='font-bold text-5xl md:text-9xl  '>{price}$</p>
+                    <div className='flex flex-row   gap-5'>
+                        <p className='font-bold text-4xl md:text-9xl  '>{price}$</p>
                         <p className='text-green-600 pt-0 md:pt-5  text-2xl md:text-6xl fort-semibold '>{discountPercentage}% off</p>
                     </div>
 
                     <p className=' text-2xl md:text-4xl pt-2 md:pt-5'>Inclusive of all taxes</p>
 
                     <div className='flex flex-wrap md:flex-none text-center text-sm  md:text-3xl gap-5 md:gap-16 mt-8 md:mt-20'>
-                        <div className=' rounded-xl bg-green-400 h-[70px] w-[100px] md:h-[100px] md:w-[200px]'>
-                            <p className='text-center'>{availabilityStatus}</p>
+                        <div className=' pt-4 rounded-xl bg-green-400 h-[50px] w-[140px] md:h-[100px] md:w-[200px]'>
+                            <p >{availabilityStatus}</p>
                         </div>
 
-                        <div className=' p-2 rounded-xl bg-green-400 h-[70px] w-[100px] md:h-[100px] md:w-[200px]'>
+                        <div className=' p-2 rounded-xl bg-green-400 h-[50px] w-[140px] md:h-[100px] md:w-[200px]'>
                             <p>{shippingInformation}</p>
                         </div>
 
-                        <div className='p-2 rounded-xl bg-green-400 h-[70px] w-[100px] md:h-[100px] md:w-[200px]'>
+                        <div className='p-2 rounded-xl bg-green-400 h-[50px] w-[140px] md:h-[100px] md:w-[200px]'>
                             <p>{warrantyInformation}</p>
                         </div>
-                        <div className='p-2 rounded-xl bg-green-400 h-[70px] w-[100px] md:h-[100px] md:w-[200px]'>
+                        <div className='p-2 rounded-xl bg-green-400 h-[50px] w-[140px] md:h-[100px] md:w-[200px]'>
                             <p>{returnPolicy}</p>
                         </div>
 
--- src/Component/Reviews.jsx
+++ src/Component/Reviews.jsx
@@ -14,11 +14,11 @@ const Reviews = ({ reviews, rating }) => {
                 {reviews && (
                     reviews.map((item) => {
                         return (
-                            <div className='mt-5 md:mt-10   shadow-2xl h-52 md:h-64 w-full md:w-[80%]  '>
-                                <p className='flex bg-green-600  text-center text-xl md:text-3xl px-4 py-1 md:py-3 font-bold rounded-3xl w-24'>{item.rating}<img className='h-9 mr-4 w-9' src='https://png.pngtree.com/png-vector/20240125/ourmid/pngtree-white-star-png-png-image_11494271.png' /></p>
-                                 <p className='text-2xl md:text-5xl pt-4 md:pt-10'>{item.comment}</p>
-                                 <p className='pt-3 md:pt-5 text-2xl md:text-4xl text-gray-400'>{item.reviewerName}</p>
-                                 <p className='pt-3 text-gray-400 '> {item.date}</p>
+                            <div className='mt-7 md:mt-10 pl-4 shadow-2xl h-40  md:h-64 w-full md:w-[80%]  '>
+                                <p className='flex bg-green-600  text-center text-xl md:text-3xl px-4 py-0 md:py-3 font-bold rounded-2xl md:w-24 w-16'>{item.rating}<img className='h-7 md:h-9 mr-0 md:mr-4 w-6 md:w-9' src='https://png.pngtree.com/png-vector/20240125/ourmid/pngtree-white-star-png-png-image_11494271.png' /></p>
+                                 <p className='text-2xl md:text-5xl pt-2 md:pt-10'>{item.comment}</p>
+                                 <p className='pt-2 md:pt-5 text-2xl md:text-4xl text-gray-400'>{item.reviewerName}</p>
+                                 <p className='pt-2 text-gray-400 '> {item.date}</p>
                             </div>
                         )
 
